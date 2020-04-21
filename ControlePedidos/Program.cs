83b68eda0e02e19224e5e1403a3862d1ec4dbf20
Evandro Padrao <evandropadrao@gmail.com>
2020-04-21 00:11:03 -0300

    Versao inicial - Controle de Pedidos

---
+++ ControlePedidos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ControlePedidos.Entities;
+using ControlePedidos.Entities.Enums;
+
+namespace ControlePedidos
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Order order = new Order();
+         order.Id = 100;
+         order.Moment = DateTime.Now;
+         order.Status = OrderStatus.PendingPayment;
+
+         Console.WriteLine(order);
+
+         Order newOrder = new Order
+         {
+            Id = 101,
+            Moment = DateTime.Now.AddDays(1),
+            Status = OrderStatus.Processing
+         };
+
+         Console.WriteLine(newOrder);
+         newOrder.Status = Enum.Parse<OrderStatus>("Shipped");
+         Console.WriteLine(newOrder);
+      }
+   }
+}
