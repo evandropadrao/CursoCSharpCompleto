b0bdcedb804a67d1e5e9c16278d04b49f15f154e
Evandro Padrao <evandropadrao@gmail.com>
2020-04-25 12:06:48 -0300

    Exercicio Interface - inclusao de contrato EUA e melhorias no codigo

--- Exercicio_Interface/Program.cs
+++ Exercicio_Interface/Program.cs
@@ -17,22 +17,47 @@ namespace Exercicio_Interface
          Console.Write("Date (dd/MM/yyyy): ");
          DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
          Console.Write("Contract value: ");
-         double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-         Contract contract = new Contract(number, date, value);
+         double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
          Console.Write("Enter number of installments: : ");
          int installments = int.Parse(Console.ReadLine());
 
+         BrazilianContract(number, date, amount, installments);
+
+         USContract(number, date, amount, installments);
+      }
+
+      static void BrazilianContract(int number, DateTime date, double amount, int installments)
+      {
+         Console.WriteLine();
+         Console.WriteLine("Brazilian Contract: ");
+
+         Contract contract = new Contract(number, date, amount);
+
          ContractService contractService = new ContractService(new PaypalService());
          contractService.ProcessContract(contract, installments);
 
+         ShowInstallments(contract.GetInstallments());
+      }
 
+      static void USContract(int number, DateTime date, double amount, int installments)
+      {
          Console.WriteLine();
+         Console.WriteLine("US Contract: ");
+
+         Contract contract = new Contract(number, date, amount);
+
+         ContractService contractService = new ContractService(new USPaylpalService());
+         contractService.ProcessContract(contract, installments);
+
+         ShowInstallments(contract.GetInstallments());
+      }
+
+      static void ShowInstallments(List<Installment> installments)
+      {
          Console.WriteLine("Installments: ");
 
-         List<Installment> listOfInstallments = contract.GetInstallments();
-         foreach (var installment in listOfInstallments)
+         foreach (var installment in installments)
          {
             Console.WriteLine(installment);
          }
--- Exercicio_Interface/Services/USPaylpalService.cs
+++ Exercicio_Interface/Services/USPaylpalService.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Exercicio_Interface.Entitites;
-using Exercicio_Interface.Services;
 
 namespace Exercicio_Interface.Services
 {
-   class PaypalService : IOnlinePaymentService
+   class USPaylpalService : IOnlinePaymentService
    {
-      private const double InterestRate = 0.01;
-      private const double FeeRate = 0.02;
+      private const double InterestRate = 0.005;
+      private const double FeeRate = 0.015;
 
       double IOnlinePaymentService.Interest(double amount, int months)
       {
